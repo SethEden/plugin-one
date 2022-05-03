cfcb415339e75254b12439fb5573037f26503625
Seth Hollingsead <Iceversaka@hotmail.com>
2022-05-03 08:03:46 -0500

    Push changes so they don't get lost. This will most likely change, when we get to plugin MVP interface on haystacks.

--- src/index.js
+++ src/index.js
@@ -7,3 +7,8 @@
  * @date 2021/06/29
  * @copyright Copyright © 2020-… by Seth Hollingsead. All rights reserved
  */
+function bootStrapPlugin() {
+  console.log('BEGIN index.bootStrapPlugin function');
+
+  console.log('END index.bootStrapPlugin function');
+};
